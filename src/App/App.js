60767b0c099b47a82d6ea89e203bf7abedea5e6c
Keith <kayzee@live.com>
2019-11-21 21:33:34 -1000

    setup frontend boilerplate

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Yoyo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
