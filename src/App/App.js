63822f0688f87133f59b5562afb469a1534b1100
Keith <kayzee@live.com>
2019-11-23 09:50:35 -1000

    adding redux and react-redux  package.json

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
@@ -18,4 +19,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(App);
