2ae71f5956a46601f35600399c55edb6da932c49
Keith <kayzee@live.com>
2019-11-23 17:18:53 -1000

    added header component

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import Header from '../containers/Header/Header';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {}
+    console.log('[App.js] constructor');
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Yoyo</h1>
-        </header>
+        <Header />
       </div>
     )
   }
 }
 
-export default connect(App);
+const mapStateToProps = () => {
+  console.log('[App.js] mapStateToProps');
+  return {}
+}
+const mapDispatchToProps = () => {
+  console.log('[App.js] mapDispatchToProps');
+  return {}
+}
+
+App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default App;
