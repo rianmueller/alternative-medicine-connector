31453c5688b1db3fe0fad585d71e44dc658398c4
Keith <kayzee@live.com>
2019-12-07 09:04:52 -1000

    article css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = props => (
     <ul>
       <li><a href='/home'>Home</a></li>
       <li><a href='/studies'>Studies</a></li>
-      <li><a href='/news'>News</a></li>
+      <li><a href='/articles'>Articles</a></li>
       <li><a href='/strains'>Strains</a></li>
       <li><a href='/chat'>Chat</a></li>
     </ul>
