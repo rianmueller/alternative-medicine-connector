5ba082a4021605073d64d62c47f5874ddb66a2f2
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-12-27 15:30:09 -1000

    removes strains link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import styles from './Sidebar.module.css';
+import React from "react";
+import styles from "./Sidebar.module.css";
 
 const Sidebar = props => (
   <div className={styles.sideDrawer}>
     <div className={styles.Home}>
-      <a href='/' >Home</a>
+      <a href="/">Home</a>
     </div>
     <div className={styles.Studies}>
-      <a href='/studies' >Studies</a>
+      <a href="/studies">Studies</a>
     </div>
     <div className={styles.Articles}>
-      <a href='/articles' >Articles</a>
-    </div>
-    <div className={styles.Strains}>
-      <a href='/strains' >Strains</a>
+      <a href="/articles">Articles</a>
     </div>
     <div className={styles.Chat}>
-      <a href='/chat' >Chat</a>
+      <a href="/chat">Chat</a>
     </div>
-
   </div>
-)
+);
 
 export default Sidebar;
